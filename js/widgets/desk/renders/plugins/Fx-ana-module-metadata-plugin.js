4b4da205dab2b809a4af31b1810b51f529849a1e
fabriziofenix <fabrizio.castelli@fao.org>
2015-10-23 16:08:02 +0200

    added  metadata export on plugin metadata

--- js/widgets/desk/renders/plugins/Fx-ana-module-metadata-plugin.js
+++ js/widgets/desk/renders/plugins/Fx-ana-module-metadata-plugin.js
@@ -3,15 +3,19 @@
 define([
     'jquery',
     'underscore',
+    'config/Config',
     'FENIX_UI_METADATA_VIEWER',
+    'fx-report',
     'text!fx-ana/html/widgets/desk/plugins/metadata-template.html'
-], function ($, _, METADATDA, template) {
+], function ($, _, C,METADATDA,Report, template) {
 
     'use strict';
 
     var defaultOptions = {
         interaction: "click",
-        label: 'Metadata'
+        label: 'Metadata',
+        BTN_EXPORT_METADATA : '.fx-md-report-btn',
+        METADATA_PANEL : '.metadata_panel'
     };
 
     function MetadataPlugin(options) {
@@ -75,13 +79,17 @@ define([
         this.$el.append( t );
 
         /* Metadata. */
+        this.$report = new Report();
+        this.$report.init('metadataExport')
         var metadata = new METADATDA();
         metadata.init({
             data: this.model.metadata,
             lang: 'en',
             //placeholder_id: 'metadata_panel',
-            placeholder : this.$el.find('.metadata_panel')
+            placeholder : this.$el.find(defaultOptions.METADATA_PANEL)
         });
+
+        this._listenToExportMetadata();
 
     };
 
@@ -94,6 +102,30 @@ define([
         }
 
         return this;
+    };
+
+    MetadataPlugin.prototype._listenToExportMetadata = function() {
+        var fileName = this.model.metadata.title['EN'].replace(/[^a-z0-9]/gi, '_').toLowerCase();
+
+        var self = this;
+        $(defaultOptions.BTN_EXPORT_METADATA, defaultOptions.METADATA_PANEL).on('click', function(){
+
+            var payload = {
+                input:{
+                    config:{
+                        uid: self.model.metadata.uid
+                    }
+                },
+                output: {
+                    config:{
+                        lang : 'en'.toUpperCase(),
+                        fileName: fileName+'.pdf'
+                    }
+                }
+            };
+
+            self.$report.exportData(payload,C.MD_EXPORT_URL);
+        });
     };
 
     return MetadataPlugin;

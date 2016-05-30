1fc57c94c0c13a9fca33946b36204e79e9dee760
mentaltoy <Nicola.Selleri@fao.org>
2016-05-30 14:02:18 +0200

    .

--- i18n/root/analysis.js
+++ i18n/root/analysis.js
@@ -28,7 +28,10 @@ define(function () {
         request : "Request error",
         empty_values : "Empty selection",
 
-        courtesy_intro: "Welcome to the Fenix Platform Analysis Section. <br>Here you will be able to produce statistic analysis by using all the data stored in the Fenix Network.<br> Please start with your first AVB (Analysis Visualization Box) by choosing a resource from the catalog by clicking the button above."
+        courtesy_intro_title: "Welcome to the Fenix Platform Analysis Section",
+
+
+        courtesy_intro: "Here you will be able to produce statistic analysis by using all the data stored in the Fenix Network.<br> Please start with your first AVB (Analysis Visualization Box) by choosing a resource from the catalog by clicking the button above."
 
     }
 });
